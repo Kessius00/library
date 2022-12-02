35647298bc9493eadfb71bf7cd87079ba9080680
kessius00 <kes.olling00@gmail.com>
2022-12-02 01:51:36 +0100

    Add newBookForm and style sidebar

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-function Book(title, author, pages){
+function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
+}
+
+//Prototypes
+Book.prototype.haveRead = function(){
+    return (this.read) ?  "Have read." :  "Haven't read.";
 }
 
 Book.prototype.info = function(){
-    return (this.title) + ' by ' + this.author + ' with ' + this.pages + ' pages.'
+    return (this.title) + ' by ' + this.author + ' with ' + this.pages + ' pages. ' + this.haveRead();
 }
 
-const Hobbit = new Book('The Hobbit', 'Tolkien', 208);
+const Hobbit = new Book('The Hobbit', 'Tolkien', 208, false);
 
-console.log(Hobbit.info())
+
+let myLibrary = [];
+
+function addBookToLibrary(){
+
+}
+
+
+const addBookBtn = document.querySelector('.addBook');
+
+addBookBtn.addEventListener('click', ()=>{
+    
+})
+
