d85b29d11896afe3badd761ffa118b6d210d1749
kessius xubuntu <kes.olling00@gmail.com>
2023-03-11 02:32:02 +0100

    make popUpform work (form action sends to svg)

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const newBtn = document.getElementsByClassName('new_book');
+
+
+function openForm(){
+    document.getElementById("popUpform").style.display = 'block';
+}
+
+
+// newBtn.addEventListener('click', openForm());
+
+
+
+function closeForm(){
+    document.getElementById('popUpform').style.display = 'none';
+}
