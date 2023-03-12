4e99bca5b02c4dad2c8055589141cd0825b39595
kessius00 <kes.olling00@gmail.com>
2023-03-12 21:46:02 +0100

    minor changes

--- script.js
+++ script.js
@@ -1,85 +1,75 @@
 // constants
 
-const bookGrid = document.querySelector('.book-grid')
+const bookGrid = document.querySelector(".book-grid");
 // clickable
-const addBookBtn = document.querySelector('.addBook');
+const addBookBtn = document.querySelector(".addBook");
 
-const totalForm = document.querySelector('.newBookForm');
-const formCancel = document.querySelector('#cancel');
-const formSubmit = document.querySelector('#submit');
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const completedPagesInput = document.querySelector('#completedPages');
-const totalPagesInput = document.querySelector('#totPages');
-const fullyReadCheckbox = document.querySelector('#fullyRead');
+const totalForm = document.querySelector(".newBookForm");
+const formCancel = document.querySelector("#cancel");
+const formSubmit = document.querySelector("#submit");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const completedPagesInput = document.querySelector("#completedPages");
+const totalPagesInput = document.querySelector("#totPages");
+const fullyReadCheckbox = document.querySelector("#fullyRead");
 
-
-
-const editBookInfo = document.querySelector('#editBookInfo');
-
+const editBookInfo = document.querySelector("#editBookInfo");
 
 // changable
-const amountOfBooks = document.querySelector('.amountBooks p');
-const completedBooks = document.querySelector('.booksCompleted p');
+const amountOfBooks = document.querySelector(".amountBooks p");
+const completedBooks = document.querySelector(".booksCompleted p");
 
+console.log(bookGrid.childElementCount);
 
+formCancel.addEventListener("click", closeTheForm);
+addBookBtn.addEventListener("click", openForm);
 
-console.log(bookGrid.childElementCount)
-
-
-formCancel.addEventListener('click', closeTheForm);
-addBookBtn.addEventListener('click', openForm);
-
-
-function alterBookAmount(){
-    amountOfBooks.textContent = bookGrid.childElementCount;
-    
+function alterBookAmount() {
+  amountOfBooks.textContent = bookGrid.childElementCount;
 }
 
 function closeTheForm() {
-    totalForm.style.display = "none";
-    document.querySelector('.backgroundBlur').style.display = 'none';
-    alterBookAmount();
-
+  totalForm.style.display = "none";
+  document.querySelector(".backgroundBlur").style.display = "none";
+  alterBookAmount();
 }
 
-
-
-function openForm(){
-    totalForm.style.display = 'block';
-    document.querySelector('.backgroundBlur').style.display = 'block';
+function openForm() {
+  totalForm.style.display = "block";
+  document.querySelector(".backgroundBlur").style.display = "block";
 }
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 //Prototypes
-Book.prototype.haveRead = function(){
-    return (this.read) ?  "Have read." :  "Haven't read.";
-}
+Book.prototype.haveRead = function () {
+  return this.read ? "Have read." : "Haven't read.";
+};
 
-Book.prototype.info = function(){
-    return (this.title) + ' by ' + this.author + ' with ' + this.pages + ' pages. ' + this.haveRead();
-}
+Book.prototype.info = function () {
+  return (
+    this.title +
+    " by " +
+    this.author +
+    " with " +
+    this.pages +
+    " pages. " +
+    this.haveRead()
+  );
+};
 
-const Hobbit = new Book('The Hobbit', 'Tolkien', 208, false);
-
+const Hobbit = new Book("The Hobbit", "Tolkien", 208, false);
 
 let myLibrary = [];
 
-function addBookToLibrary(){
-
-}
+function addBookToLibrary() {}
 
 // nice form function: the form reset function (form.reset()) - to reset the form
 // create a formData object, iterate over it with forof loop
 
-
-addBookBtn.addEventListener('click', ()=>{
-    
-})
-
+addBookBtn.addEventListener("click", () => {});
